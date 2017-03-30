ff44e16c21ee551060e2cde2ce65415aa6f3e4af
Alimjan <maninwindow@hotmail.com>
2017-03-30 19:38:04 +0800

    HasuniqueCharacters practice.

---
+++ CrackTheInterviewQuestions/Array/HasUniqueValue_UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using CrackTheInterviewQuestions_UnitTest.Array;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackTheInterviewQuestions.Array
+{
+    [TestClass]
+    public class HasUniqueValue_UnitTest
+    {
+        [TestMethod]
+        public void HasUniqueCharacters_Test()
+        {
+            //Arrange
+            HasUniqueValue hasUniqueValue = new HasUniqueValue();
+            string text = "Lets check if has multiple character.";
+
+            //Act
+            bool result = hasUniqueValue.HasuniqueCharacters(text);
+
+            //Assert
+            Assert.IsFalse(result); ;
+        }
+
+        [TestMethod]
+        public void HasUniqueValueSpaceEfficient_Test()
+        {
+            //Arrange
+            HasUniqueValue hasUniqueValue = new HasUniqueValue();
+            string text = "Lets check if has multiple character.";
+
+            //Act
+            bool result = hasUniqueValue.HasUniqueValueSpaceEfficient(text);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
--- CrackTheInterviewQuestions_UnitTest/Array/HasUniqueValue.cs
+++ CrackTheInterviewQuestions_UnitTest/Array/HasUniqueValue.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace CrackTheInterviewQuestions_UnitTest.Array
 {
-    class HasUniqueValue
+    public class HasUniqueValue
     {
+        public bool HasuniqueCharacters(string text)
+        {
+            if (text.Length > 256)
+                return false;
+            Char[] textArray = text.ToArray();
+            Char[] chars = new Char[256];
+            Logger.LogStart();
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                chars[textArray[i]]++;
+                if (chars[textArray[i]] == 2)
+                    Logger.LogStart(); return false;
+            }
+            return true;
+        }
+
+        public bool HasUniqueValueSpaceEfficient(string text)
+        {
+            if (text.Length > 256)
+                return false;
+            Char[] textArray = text.ToArray();
+            Logger.LogStart();
+            for (int i = 0; i < textArray.Length; i++)
+                for (int j = i + 1; j < textArray.Length; j++)
+                {
+                    if (textArray[i] == textArray[j])
+                    {
+                        Logger.LogEnd();
+                        return false;
+                    }
+                }
+            Logger.LogEnd();
+            return true;
+        }
     }
 }
